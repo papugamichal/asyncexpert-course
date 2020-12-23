9b6da6bb917bfc46f7299f13383cc7d285c2ba16
Michal Papuga <mpapuga@prescientco.com>
2020-12-23 13:47:18 +0100

    Complete Module02

--- Module02-AsyncBasics/ThreadPoolExercises.Core/ThreadingHelpers.cs
+++ Module02-AsyncBasics/ThreadPoolExercises.Core/ThreadingHelpers.cs
@@ -12,8 +12,31 @@ namespace ThreadPoolExercises.Core
             // * In a loop, check whether `token` is not cancelled
             // * If an `action` throws and exception (or token has been cancelled) - `errorAction` should be invoked (if provided)
 
+            var thread = new Thread(DoJob);
+            thread.Start();
+            thread.Join();
+            void DoJob()
+            {
+                int loop = 0;
 
-
+                try
+                {
+                    do
+                    {
+                        loop += 1;
+                        if (token.IsCancellationRequested)
+                        {
+                            throw new OperationCanceledException();
+                        }
+                        action?.Invoke();
+                    }
+                    while (loop < repeats);
+                }
+                catch (Exception e)
+                {
+                    errorAction?.Invoke(e);
+                }
+            }
         }
 
         public static void ExecuteOnThreadPool(Action action, int repeats, CancellationToken token = default, Action<Exception>? errorAction = null)
@@ -23,8 +46,32 @@ namespace ThreadPoolExercises.Core
             // * In a loop, check whether `token` is not cancelled
             // * If an `action` throws and exception (or token has been cancelled) - `errorAction` should be invoked (if provided)
 
+            using var finished = new AutoResetEvent(false);
+            ThreadPool.QueueUserWorkItem(DoJob);
+            finished.WaitOne();
 
+            void DoJob(object? state)
+            {
+                int loop = 0;
 
+                try
+                {
+                    do
+                    {
+                        loop += 1;
+
+                        token.ThrowIfCancellationRequested();
+                        action?.Invoke();
+                    }
+                    while (loop < repeats);
+                }
+                catch (Exception e)
+                {
+                    errorAction?.Invoke(e);
+                }
+
+                finished.Set();
+            }
         }
     }
 }
